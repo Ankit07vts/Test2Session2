09d4c2bdc028ba9d9eface32a1395428452d8f2b
Ankitvats <95743251+Ankit07vts@users.noreply.github.com>
2022-02-23 10:03:59 +0000

    .

--- p1original.c
+++ p1original.c
@@ -1,2 +1,32 @@
+#include <stdio.h>
+#include <math.h>
 
+void input(float*x1 , float*x2 , float*y1 , float*y2)
+{
+  printf("Enter a point \n");
+  scanf("%f%f\n",x1,x2);
+  printf("Enter a point\n");
+  scanf("%f%f",y1,y2);   
+}
+
+void distance(float x1,float x2,float y1,float y2,float*d)
+{
+  float x,y;
+  x=x1-x2;
+  y=y1-y2;
+  *d=sqrt(x*x+y*y); 
+}
+
+void output(float x1,float x2,float y1,float y2,float d)
+{
+  printf("the distance between poinr (%f,%f) and (%f,%f) is %f\n",x1,x2,y1,y2,d);
+  
+}
+int main()
+{
+  float x1,x2,y1,y2,d;
+  input(&x1,&x2,&y1,&y2);
+  distance(x1,x2,y1,y2,&d);
+  output(x1,x2,y1,y2,d);
+  return 0; 
 }
---
+++ p4original.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int input()
+{
+  int n;
+  printf("Enter a number\n");
+  scanf("%d",&n);
+  return n;
+}
+int find_fibo(int n)
+{
+  
+}
